d3181e8c43393cc4c82e6c32ddf8c41407cc50aa
ele10-code <elena.zerbin.ez@gmail.com>
2024-07-16 20:51:43 +0200

    update for pipelines

--- db_config.py
+++ db_config.py
@@ -4,6 +4,6 @@ db_config = {
     'user': os.getenv('DB_USER', 'admin'),
     'password': os.getenv('DB_PASSWORD', 'admin_password'),
     'host': os.getenv('DB_HOST', 'localhost'),
-    'database': os.getenv('DB_NAME', 'GestioneProgetti'),
+    'database': os.getenv('DB_NAME', 'TestGestioneProgetti'),
     'port': os.getenv('DB_PORT', '3306')
 }
